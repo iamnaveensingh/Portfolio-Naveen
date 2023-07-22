64b7d76eb90a4ec51d5b6afa72c36c80804d3481
Naveen Singh <naveensingh7604@gmail.com>
2023-07-22 18:35:03 +0530

    Update

--- src/components/Main.js
+++ src/components/Main.js
@@ -19,7 +19,7 @@ const Home = () => {
           organization's success.
         </p>
         <Link
-          to=""
+          to="https://drive.google.com/file/d/166dNllQIpsbfE4eNCrxlU4Zm9F4JsyBK/view?usp=sharing"
           target="_blank"
         >
           <button className="border-[#3D9239] border-2 items-center  flex w-fit px-4 py-2 rounded-xl hover:border-pink-600 transition-all duration-500 text-[#ccd6f6]">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -115,7 +115,7 @@ const Navbar = () => {
             <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
               <a
                 className="flex justify-between items-center w-full text-gray-300"
-                href=""
+                href="https://drive.google.com/file/d/166dNllQIpsbfE4eNCrxlU4Zm9F4JsyBK/view?usp=sharing"
                 target="_blank"
                 rel="noreferrer"
               >
