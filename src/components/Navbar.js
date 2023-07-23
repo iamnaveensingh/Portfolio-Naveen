b2c0f8b9e6953b3936e9a59a84ea77c31368382b
Naveen Singh <naveensingh7604@gmail.com>
2023-07-23 20:29:10 +0530

    Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -115,7 +115,7 @@ const Navbar = () => {
             <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
               <a
                 className="flex justify-between items-center w-full text-gray-300"
-                href="https://drive.google.com/file/d/166dNllQIpsbfE4eNCrxlU4Zm9F4JsyBK/view?usp=sharing"
+                href="https://drive.google.com/file/d/17VF9vOh0l8Y3WB4_OJ8ScQlLnJKqZ06I/view?usp=sharing"
                 target="_blank"
                 rel="noreferrer"
               >
